a6ea41e6d99ae5f093a9e0375d403e349cffca34
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2010-04-04 22:50:10 +0000

    * moap/extern/command/help2man.py (added): 	  Add a man page generator from help output of command tree.

---
+++ command/help2man.py
@@ -0,0 +1,70 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+# Morituri - for those about to RIP
+
+# Copyright (C) 2009 Thomas Vander Stichele
+
+# This file is part of morituri.
+#
+# morituri is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# morituri is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with morituri.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+import optparse
+import tempfile
+import pickle
+import shutil
+import StringIO
+
+from morituri.rip import main
+
+def walk(command, level=0):
+    # print "%s%s: %s" % (" " * level, command.name, command.summary)
+    for name, c in command.subCommands.items():
+        walk(c, level + 2)
+
+def manwalk(command):
+    ret = []
+
+    ret.append(".SH %s" % command.getFullName())
+    # avoid printing the summary twice; once here and once in usage
+    if command.summary and command.description:
+        ret.append(command.summary)
+        ret.append("")
+
+    s = StringIO.StringIO()
+    command.outputHelp(file=s)
+    ret.append(s.getvalue())
+    ret.append("")
+
+    names = command.subCommands.keys()
+    names.sort()
+    for name in names:
+        c = command.subCommands[name]
+        ret.extend(manwalk(c))
+
+    return ret
+
+# ugly hack so that help output uses first argument for %prog instead of
+# 'doc.py'
+if len(sys.argv) > 1:
+    sys.argv[0] = sys.argv[1]
+
+
+man = []
+man.append('.TH "rip" "1" "October 2009"')
+man.extend(manwalk(main.Rip(width=70)))
+
+print "\n".join(man)
